c28f40fe91dd70a83083c01566ddb0d47752c486
Andrea <andrea.africa93@gmail.com>
2017-01-27 17:45:43 -0500

    initial commit, issues with datepicker delete function not resolved

---
+++ crm.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    var max_leads           = 10;
+    var max_calls           = 10;
+    var max_appointments    = 10;
+
+
+    var lead = 1;
+    $('.add-lead').on('click', function(e) {
+      e.preventDefault();
+      if(lead < max_leads){
+        lead++;
+        $('.lead-container').append('<div><input type="text" name="add-name" placeholder="Name"/><input type="tel" name="add-number"placeholder="Telephone"/><a href="#" class="remove_field">Delete</a></div>');
+      }
+    });
+
+    $('.lead-container').on('click','.remove_field', function(e){
+      e.preventDefault();
+      $(this).parent('div').remove();
+      lead--;
+    })
+
+    var call = 1;
+    $('.log-calls').on('click', function(e) {
+      e.preventDefault();
+      if(call < max_calls) {
+        call++;
+        $('.calls-container').append('<div><input type="text" name="add-name" placeholder="Name"/><input type="tel" name="add-number" placeholder="Telephone"/><input type="date" name="add-date" placeholder="Date"/><input type="text" name="add-agent" placeholder="Assigned To"/><input type="text" name="add-notes" placeholder="Notes"/><a href="#" class="remove_field">Delete</a></div>');
+      }
+    });
+
+    $('.calls-container').on('click', function(e) {
+      e.preventDefault();
+      $(this).parent('div').remove();
+      call--;
+    })
+
+
+    $(function() {
+      $('#datepicker').datepicker({
+        altField: '#alternate',
+        altFormat: 'DD, d MM, yy'
+      });
+    });
+
+
+    var appointment = 1;
+    $('.appointment-container').on('click', function(e) {
+      e.preventDefault();
+      if(appointment < max_appointments) {
+        appointment++;
+        $('.appointment-container').append('<div><input class="appointment-dates" type="text" class="date-cal" placeholder="Pick a Date"/><a href="#" class="remove_field">Delete</a></div>');
+      }
+    });
+
+    $('.appointment-container').on('click', function(e) {
+      e.preventDefault();
+      $(this).parent('.appointment-dates').remove();
+      appointment--;
+    })
+
+
+});
