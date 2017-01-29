163eab0d93743214897e3d36f16a90b8bfead774
Andrea <andrea.africa93@gmail.com>
2017-01-29 13:20:00 -0500

    fixed issues with appointment delete button destroying default input

--- crm.js
+++ crm.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       e.preventDefault();
       if(lead < max_leads){
         lead++;
-        $('.lead-container').append('<div><input type="text" name="add-name" placeholder="Name"/><input type="tel" name="add-number"placeholder="Telephone"/><a href="#" class="remove_field">Delete</a></div>');
+        $('.lead-container').append('<div><input type="text" name="add-name" placeholder="Name"/><input type="tel" name="add-number"placeholder="Telephone"/><button class="add-account">Add to Account</button><button class="remove_field">Delete Lead</button></div>');
       }
     });
 
@@ -25,7 +25,7 @@ $(document).ready(function() {
       e.preventDefault();
       if(call < max_calls) {
         call++;
-        $('.calls-container').append('<div><input type="text" name="add-name" placeholder="Name"/><input type="tel" name="add-number" placeholder="Telephone"/><input type="date" name="add-date" placeholder="Date"/><input type="text" name="add-agent" placeholder="Assigned To"/><input type="text" name="add-notes" placeholder="Notes"/><a href="#" class="remove_field">Delete</a></div>');
+        $('.calls-container').append('<div><input type="text" name="add-name" placeholder="Name"/><input type="tel" name="add-number" placeholder="Telephone"/><input type="date" name="add-date" placeholder="Date"/><input type="text" name="add-agent" placeholder="Assigned To"/><input type="text" name="add-notes" placeholder="Notes"/><button class="remove_field">Delete</button></div>');
       }
     });
 
@@ -49,7 +49,7 @@ $(document).ready(function() {
       e.preventDefault();
       if(appointment < max_appointments){
         appointment++;
-        $('.appointment-fields').append('<div><input class="appointment-dates" id="datepicker'+appointment+'" type="text" class="date-cal" placeholder="Appointment"/>&nbsp;<input type="text" id="alternate'+appointment+'" size="30"><a href="#" class="remove_field">Delete</a></div>');
+        $('.appointment-container').append('<div><input class="appointment-dates" id="datepicker'+appointment+'" type="text" class="date-cal" placeholder="Appointment"/>&nbsp;<input type="text" id="alternate'+appointment+'" size="30"><button class="remove_field">Delete</button></div>');
         $('#datepicker'+appointment).datepicker({
           altField: '#alternate'+appointment,
           altFormat: 'DD, d MM, yy'
@@ -57,7 +57,7 @@ $(document).ready(function() {
       }
     });
 
-    $('.appointment-fields').on('click','.remove_field', function(e){
+    $('.appointment-container').on('click','.remove_field', function(e){
       e.preventDefault();
       $(this).parent('div').remove();
       appointment--;
